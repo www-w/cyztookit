c7cb170f5db2cfe40801649a9fa50ca012e18380
cyz <zcyhun@gmail.com>
2020-11-20 19:23:23 +0800

    ignore .cxx folder

---
+++ cyztookit/src/com/example/android/helloactivity/CameraActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.helloactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
