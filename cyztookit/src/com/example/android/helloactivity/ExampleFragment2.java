8c473d6335f7c9dbded0a8cc756c0f4fc477925d
zcyhun <zcyhun@gmail.com>
2021-02-02 07:30:58 +0800

    first fragment test

--- cyztookit/src/com/example/android/helloactivity/ExampleFragment2.java
+++ cyztookit/src/com/example/android/helloactivity/ExampleFragment2.java
@@ -4,11 +4,11 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.os.Bundle;
 
-public static class ExampleFragment extends Fragment{
+public static class ExampleFragment2 extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container,
             Bundle savedInstanceState){
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.example_fragment,container,false);
+        return inflater.inflate(R.layout.example_fragment2,container,false);
     }
 }
---
+++ cyztookit/src/com/example/android/helloactivity/ExampleFragmentActivity.java
@@ -0,0 +1,25 @@
+package com.example.android.helloactivity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+
+public class ExampleFragmentActivity extends ActivityCompat{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragmenttest);
+    }
+
+}
