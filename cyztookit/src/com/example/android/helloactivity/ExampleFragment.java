13e4e0ac6e3a7acf7e80561826b23b7c481740c0
zcyhun <zcyhun@gmail.com>
2021-02-02 09:41:45 +0800

    fixbug

--- cyztookit/src/com/example/android/helloactivity/ExampleFragment.java
+++ cyztookit/src/com/example/android/helloactivity/ExampleFragment.java
@@ -3,7 +3,7 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
-
+import android.view.View;
 public class ExampleFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container,
