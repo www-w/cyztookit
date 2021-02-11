e9c54610c9016e9fb9eb1d7051f03311f92edec6
zcyhun <zcyhun@gmail.com>
2021-02-11 08:35:24 +0800

    add frag btn

--- cyztookit/src/com/example/android/helloactivity/ExampleFragmentActivity.java
+++ cyztookit/src/com/example/android/helloactivity/ExampleFragmentActivity.java
@@ -12,14 +12,33 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class ExampleFragmentActivity extends AppCompatActivity{
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragmenttest);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_fragmenttest);
+		initView();
+	}
+	void initView(){
+		Button btnAdd = findViewById(R.id.fragtestbtnadd);
+		Button btnReplace= findViewById(R.id.fragtestbtnreplace);
+		btnAdd.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View view){
+				FragmentManager fragmentManager = getSupportFragmentManager();
+				FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+				ExampleFragment fragment = new ExampleFragment();
+				fragmentTransaction.add(R.id.fragleft, fragment);
+				fragmentTransaction.commit();
+			}
+
+		});
+	}
 
 }
