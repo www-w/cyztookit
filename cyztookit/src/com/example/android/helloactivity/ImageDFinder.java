9a6609094eaee7c4c2ad3e75c108b3b06b36e568
zcyhun <zcyhun@gmail.com>
2021-02-13 09:20:55 +0800

    fix

--- cyztookit/src/com/example/android/helloactivity/ImageDFinder.java
+++ cyztookit/src/com/example/android/helloactivity/ImageDFinder.java
@@ -16,9 +16,12 @@ public class ImageDFinder{
             int row=startY+r;
             int row2=(int)Math.round(scale*r)+startY2;
             for(int col=0;col<bi.getWidth();col++){
-                if(rgbDeference(bi.getPixel(col,row),bi.getPixel(col,row2))>50){
+                if(rgbDeference(bi.getPixel(col,row),bi.getPixel(col,row2))>30){
                     bi.setPixel(col,row,Color.RED);
                 }
+		else{
+			bi,setPixel(col,row,bi.getPixel(col,row)&0xf0ffffff);
+		}
             }
         }
         return bi;
