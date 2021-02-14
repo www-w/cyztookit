853e35ea58613d1c75cea18ae5dece5b3dff9930
zcyhun <zcyhun@gmail.com>
2021-02-14 11:20:47 +0800

    fix

--- cyztookit/src/com/example/android/helloactivity/ExampleFragmentActivity.java
+++ cyztookit/src/com/example/android/helloactivity/ExampleFragmentActivity.java
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.Button;
 
+import androidx.fragment.app.Fragment;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
