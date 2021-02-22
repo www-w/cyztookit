d84eb1207ff57c595dbf7cdcd6fcb044e80cae49
zcyhun <zcyhun@gmail.com>
2021-02-22 17:42:04 +0800

    fix

--- cyztookit/src/com/example/android/helloactivity/MyViewModel.java
+++ cyztookit/src/com/example/android/helloactivity/MyViewModel.java
@@ -4,13 +4,11 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Transformations;
 import androidx.lifecycle.LiveData;
 public class MyViewModel extends ViewModel{
-	private MutableLiveData<String> txta;
+	private MutableLiveData<String> txta=new MutableLiveData<String>("init val");
 	private LiveData<String> txtb=Transformations.map(txta,a->{
 		return "trans a:"+a;
 	});
 	public MutableLiveData<String> getTxtA(){
-		if(txta==null)txta=new MutableLiveData<String>("init val");
-		txta.setValue("initB value");
 		return txta;
 	}
 	public LiveData<String> getTxtB(){
