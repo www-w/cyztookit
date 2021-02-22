86035a854b6d01e77d1d2891efa4c0d94272879d
zcyhun <zcyhun@gmail.com>
2021-02-22 16:31:02 +0800

    fix

--- cyztookit/src/com/example/android/helloactivity/MyViewModel.java
+++ cyztookit/src/com/example/android/helloactivity/MyViewModel.java
@@ -1,7 +1,7 @@
 package com.example.android.helloactivity;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.MutableLiveData;
-class MyViewModel extends ViewModel{
+public class MyViewModel extends ViewModel{
 	private MutableLiveData<String> txta;
 	public MutableLiveData<String> getTxtA(){
 		if(txta==null)txta=new MutableLiveData<String>();
